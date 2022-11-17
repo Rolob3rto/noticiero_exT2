05c9b03b3323c38fa82a0d6330783f3e47739744
Rolob3rto <robertooli98@gmail.com>
2022-11-17 10:37:30 +0100

    commit inicial

---
+++ src/test/java/com/noticiero/noticiero/NoticieroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noticiero.noticiero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticieroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
